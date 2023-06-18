fb8b715dd4cc3b373cd143f04bdb335d800fb531
Axel Luguercio <axel@Laptop-de-Axel.local>
2023-06-18 18:09:48 -0300

    avanzando

---
+++ js/session.js
@@ -0,0 +1,12 @@
+// Verificar si el usuario está autenticado (ejemplo con una variable booleana)
+const isAuthenticated = false; 
+
+// Obtener la referencia a la sección restringida
+const restrictedArea = document.getElementById('restricted-area');
+
+// Mostrar u ocultar la sección dependiendo del estado de autenticación
+if (isAuthenticated) {
+  restrictedArea.style.display = 'block'; // Mostrar la sección
+} else {
+  restrictedArea.style.display = 'none'; // Ocultar la sección
+}
